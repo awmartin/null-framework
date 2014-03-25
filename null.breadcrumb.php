d6457b2bd83f0a62e8869511673cfe0157fbabfa
William Martin <awmartin@gmail.com>
2014-03-25 17:48:30 -0400

    Category list fix in breadcrumb

--- null.breadcrumb.php
+++ null.breadcrumb.php
@@ -1,7 +1,12 @@
 <?php 
 function NullBreadcrumb() {
     $tr = '<ul id="breadcrumb">';
-    $tr = $tr."<li>&middot; ".get_the_category_list( ', ' )."</li>";
+    
+    $category_list = trim(get_the_category_list( ', ' ));
+    if ($category_list != "") {
+        $tr = $tr."<li>&middot; ".get_the_category_list( ', ' )."</li>";
+    }
+    
     $tr = $tr.'<li>&middot; <a href="'.get_site_url().'">Home</a></li>';
     $tr = $tr.'</ul>';
     return $tr;
